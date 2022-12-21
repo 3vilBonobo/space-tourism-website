c047d1b3e30b7f671ecba4ddd68cde8ce93102d3
3vilBonobo <iboutsika@gmail.com>
2022-12-21 18:23:30 +0200

    add basic header structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import Navbar from './Navbar';
+import {
+  StyledHeader,
+  StyledLogoLink,
+  StyledLogo,
+  StyledSeparator,
+} from '../styles/Header.style';
+import Logo from '../assets/shared/logo.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <StyledHeader>
+      <StyledLogoLink>
+        <StyledLogo src={Logo} />
+      </StyledLogoLink>
+      <StyledSeparator />
+      <Navbar />
+    </StyledHeader>
+  );
 };
 
 export default Header;
---
+++ src/styles/Header.style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledHeader = styled.header`
+  position: absolute;
+  top: 0px;
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
+  @media only screen and (min-width: 600px) {
+    flex-direction: row;
+  }
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const StyledLogoLink = styled(Link)``;
+export const StyledLogo = styled.img``;
+
+export const StyledSeparator = styled.hr`
+  transform: rotate(90deg);
+`;
