7adc0d8064e2ed763695fddc7292f93ec8071124
3vilBonobo <iboutsika@gmail.com>
2022-12-21 20:03:55 +0200

    add basic navbar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import {
+  StyledNavbar,
+  StyledNavList,
+  StyledNavListItem,
+  StyledNavLink,
+  StyledNavSpan,
+} from '../styles/Navbar.style';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <StyledNavbar>
+      <StyledNavList>
+        <StyledNavListItem>
+          <StyledNavLink>
+            <StyledNavSpan>00</StyledNavSpan>HOME
+          </StyledNavLink>
+        </StyledNavListItem>
+        <StyledNavListItem>
+          <StyledNavLink>
+            <StyledNavSpan>01</StyledNavSpan>DESTINATION
+          </StyledNavLink>
+        </StyledNavListItem>
+        <StyledNavListItem>
+          <StyledNavLink>
+            <StyledNavSpan>02</StyledNavSpan>CREW
+          </StyledNavLink>
+        </StyledNavListItem>
+        <StyledNavListItem>
+          <StyledNavLink>
+            <StyledNavSpan>03</StyledNavSpan>TECHNOLOGY
+          </StyledNavLink>
+        </StyledNavListItem>
+      </StyledNavList>
+    </StyledNavbar>
+  );
 };
 
 export default Navbar;
--- src/styles/Navbar.style.js
+++ src/styles/Navbar.style.js
@@ -1,1 +1,25 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+export const StyledNavbar = styled.nav`
+  display: flex;
+`;
+
+export const StyledNavList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StyledNavListItem = styled.li``;
+
+export const StyledNavLink = styled(Link)`
+  color: ${(props) => props.theme.colors.white};
+  font-family: ${(props) => props.theme.fonts.fontSansSerif};
+  text-decoration: none;
+`;
+
+export const StyledNavSpan = styled.span`
+  padding-inline: 5px;
+`;
