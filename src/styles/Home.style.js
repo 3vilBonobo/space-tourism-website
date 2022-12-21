ebdd1e26be3ee447a871629ab5ff8850e6b00b54
3vilBonobo <iboutsika@gmail.com>
2022-12-21 18:03:53 +0200

    add home styles

--- src/styles/Home.style.js
+++ src/styles/Home.style.js
@@ -5,19 +5,23 @@ import bgTablet from '../assets/home/background-home-tablet.jpg';
 import bgMobile from '../assets/home/background-home-mobile.jpg';
 
 export const StyledMainHome = styled.main`
-  height: 100vh;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  padding-inline: 200px;
+  height: 100vh;
   background-color: black;
   background-image: url(${bgMobile});
+  background-repeat: no-repeat;
+  background-size: cover;
   @media only screen and (min-width: 600px) {
     background-image: url(${bgTablet});
   }
   @media only screen and (min-width: 768px) {
     background-image: url(${bgDesktop});
+    flex-direction: row;
+    padding-inline: 200px;
+    height: 100vh;
   }
 `;
 
@@ -30,21 +34,38 @@ export const StyledLeftSectionHome = styled.section`
 
 export const StyledUpperTitleHome = styled.h2`
   font-family: ${(props) => props.theme.fonts.fontSansSerif};
-  font-size: 28px;
+  font-size: 16px;
   text-transform: uppercase;
   color: ${(props) => props.theme.colors.lightCyan};
+  @media only screen and (min-width: 600px) {
+  }
+  @media only screen and (min-width: 768px) {
+    font-size: 28px;
+  }
 `;
 export const StyledBottomTitleHome = styled.h1`
-  font-size: 150px;
+  font-size: 80px;
   text-transform: uppercase;
   color: ${(props) => props.theme.colors.white};
+  @media only screen and (min-width: 600px) {
+    font-size: 150px;
+  }
+  @media only screen and (min-width: 768px) {
+    font-size: 150px;
+  }
 `;
 export const StyledTextHome = styled.p`
   font-family: ${(props) => props.theme.fonts.fontSansSerif};
-  max-width: 430px;
-  font-size: 18px;
+  font-size: 15px;
   line-height: 32px;
   color: ${(props) => props.theme.colors.lightCyan};
+  @media only screen and (min-width: 600px) {
+    font-size: 16px;
+  }
+  @media only screen and (min-width: 768px) {
+    max-width: 430px;
+    font-size: 18px;
+  }
 `;
 
 export const StyledRightSectionHome = styled.section``;
@@ -62,4 +83,14 @@ export const StyledButtonHome = styled(Link)`
   font-family: ${(props) => props.theme.fonts.fontSerif};
   text-decoration: none;
   text-align: center;
+  @media only screen and (min-width: 600px) {
+    width: 242px;
+    height: 242px;
+    font-size: 32px;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 274px;
+    height: 274px;
+    font-size: 32px;
+  }
 `;
